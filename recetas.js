cccb48c36aa72fbfa507101cd0d3544f92667155
danielsantilli <danielsantilli1@hotmail.com>
2024-06-21 10:46:51 -0300

    mejoras en archivo recetas.js

--- recetas.js
+++ recetas.js
@@ -1,4 +1,8 @@
-function receta(ingrediente1,ingrediente2,ingrediente3,ingrediente4){
-  let receta = [ingrediente1,ingrediente2,ingrediente3,ingrediente4,];
+
+function Receta(ingrediente1, ingrediente2, ingrediente3, ingrediente4) {
+  const receta = [ingrediente1, ingrediente2, ingrediente3, ingrediente4];
   return receta;
 }
+
+const miReceta = Receta("arroz", "frijoles", "leche", "queso");
+console.log(miReceta);
